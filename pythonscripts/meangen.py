97c9694f8cbd3e76682c10ae303bd6e54c6f0a8d
avatarjasper <93929084+avatarjasper@users.noreply.github.com>
2024-03-06 10:25:50 +0100

    we can now run meangen.py

--- pythonscripts/meangen.py
+++ pythonscripts/meangen.py
@@ -1,6 +1,8 @@
 # This file will be to run meangen.exe through python.
 import subprocess
 
-
 if __name__ == "__main__":
-    subprocess.run(['ls', '-l'])
+    # Run the executable and communicate with it
+    with subprocess.Popen(['meangen-17.4.exe'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True) as process:
+        # Send input to the process
+        process.stdin.write('F\n')
