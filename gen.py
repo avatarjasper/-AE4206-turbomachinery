29a774c71fe32fcfe0c4f800eb86f30fcccc77eb
AlexRooij <131755782+AlexRooij@users.noreply.github.com>
2024-03-06 09:40:40 +0100

    total properties calculation

---
+++ gen.py
@@ -0,0 +1,15 @@
+import numpy as np
+from isa import ISA
+
+#### BASIC VALUES ###
+h_cruise = 10000
+M_cruise = 0.78
+Pr_t = 1.6
+RPM = 5000
+gamma = 1.4
+
+Ts_cruise, Ps_cruise, rho_s_cruise, mu, a = ISA(h_cruise)
+Tt_cruise = Ts_cruise*(1+(gamma-1)/2*M_cruise**2)
+Pt_cruise = Ps_cruise*(1+(gamma-1)/2*M_cruise**2)**(gamma/(gamma-1))
+
+print(Pt_cruise,Tt_cruise)
